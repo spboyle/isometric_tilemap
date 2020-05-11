ad3b4edadb340b9f5b5dbf20cc6dd50d962dc209
CodeWarriorB <spboyle7@gmail.com>
2020-05-10 21:51:18 -0600

    use code to generate tiles, need to shift colliders down on tilemap

---
+++ Assets/TileCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileCreator : MonoBehaviour {
+    // Start is called before the first frame update
+    Tilemap groundMap, floor1Map, collisionMap;
+
+    void Start() {
+        // Identify each tilemap
+        foreach (Tilemap map in GetComponentsInChildren<Tilemap>()) {
+            switch (map.name) {
+                case "Groundmap":
+                    groundMap = map;
+                    break;
+                case "Floor1map":
+                    floor1Map = map;
+                    break;
+                case "Collision":
+                    collisionMap = map;
+                    break;
+            }
+        }
+
+        drawGround();
+        drawObstacles();
+    }
+
+    void drawGround() {
+        Tile basicFloorTile = TileLoader.GetBasicFloorTile();
+
+        foreach (Vector3Int pos in groundMap.cellBounds.allPositionsWithin) {
+            groundMap.SetTile(pos, basicFloorTile);
+        }
+    }
+
+    void drawObstacles() {
+        Tile basicFloorCube = TileLoader.GetBasicFloorCube();
+        Tile collisionCube = TileLoader.GetBasicColliderCube();
+        Vector3Int[] spaces = {
+            new Vector3Int(0, 3, 0),
+            new Vector3Int(2, 8, 0),
+            new Vector3Int(4, 4, 0),
+            new Vector3Int(6, 1, 0),
+            new Vector3Int(6, 7, 0),
+            new Vector3Int(7, 2, 0),
+            new Vector3Int(9, 5, 0),
+        };
+        foreach (Vector3Int pos in spaces) {
+            floor1Map.SetTile(pos, basicFloorCube);
+            collisionMap.SetTile(pos, collisionCube);
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/TileLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class TileLoader
+{
+    private const string basicFloorTileName = "Tilemaps/Isometric/Tiles/Temple/temple-sliced_16";
+    private const string basicFloorCubeName = "Tilemaps/Isometric/Tiles/Temple/temple-sliced_26";
+    private const string colliderCubeName = "Tilemaps/Isometric/Colliders/ColliderTiles/cube08-a";
+
+    public static Tile GetBasicFloorTile()
+    {
+        return (Tile)Resources.Load(basicFloorTileName, typeof(Tile));
+    }
+
+    public static Tile GetBasicFloorCube()
+    {
+        return (Tile)Resources.Load(basicFloorCubeName, typeof(Tile));
+    }
+
+    public static Tile GetBasicColliderCube()
+    {
+        return (Tile)Resources.Load(colliderCubeName, typeof(Tile));
+    }
+}
