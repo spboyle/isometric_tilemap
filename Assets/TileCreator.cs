77668bc3516efea42e506cbf1b180ee786e33780
CodeWarriorB <spboyle7@gmail.com>
2020-05-11 19:08:06 -0600

    Randomly generate obstacles in tilemap

--- Assets/TileCreator.cs
+++ Assets/TileCreator.cs
@@ -6,10 +6,14 @@ using UnityEngine.Tilemaps;
 public class TileCreator : MonoBehaviour {
     // Start is called before the first frame update
     Tilemap groundMap, floor1Map, collisionMap;
+    private const int numObstacles = 17;
+    private const int mapSize = 15;
 
     void Start() {
         // Identify each tilemap
         foreach (Tilemap map in GetComponentsInChildren<Tilemap>()) {
+            // How to set tilemap size programmatically?
+            // map.cellBounds.SetMinMax(new Vector3Int(0, 0, 0), new Vector3Int(mapSize, mapSize, 1));
             switch (map.name) {
                 case "Groundmap":
                     groundMap = map;
@@ -23,34 +27,45 @@ public class TileCreator : MonoBehaviour {
             }
         }
 
-        drawGround();
-        drawObstacles();
+        DrawGround();
+        DrawObstacles();
     }
 
-    void drawGround() {
+    void DrawGround() {
         Tile basicFloorTile = TileLoader.GetBasicFloorTile();
 
-        foreach (Vector3Int pos in groundMap.cellBounds.allPositionsWithin) {
-            groundMap.SetTile(pos, basicFloorTile);
+        for (int mapX = 0; mapX < mapSize; mapX++) {
+            for (int mapY = 0; mapY < mapSize; mapY++) {
+                Vector3Int pos = new Vector3Int(mapX, mapY, 0);
+                groundMap.SetTile(pos, basicFloorTile);
+            }
         }
+
+        // Is there a way to set tilemap size beforehand and use cellBounds.allPositionsWithin?
+        // foreach (Vector3Int pos in groundMap.cellBounds.allPositionsWithin) {
+        //     groundMap.SetTile(pos, basicFloorTile);
+        // }
     }
 
-    void drawObstacles() {
+    void DrawObstacles() {
         Tile basicFloorCube = TileLoader.GetBasicFloorCube();
         Tile collisionCube = TileLoader.GetBasicColliderCube();
-        Vector3Int[] spaces = {
-            new Vector3Int(0, 3, 0),
-            new Vector3Int(2, 8, 0),
-            new Vector3Int(4, 4, 0),
-            new Vector3Int(6, 1, 0),
-            new Vector3Int(6, 7, 0),
-            new Vector3Int(7, 2, 0),
-            new Vector3Int(9, 5, 0),
-        };
-        foreach (Vector3Int pos in spaces) {
+
+        foreach (Vector3Int pos in GenerateObstacleCoordinates()) {
             floor1Map.SetTile(pos, basicFloorCube);
             collisionMap.SetTile(pos, collisionCube);
         }
+    }
+
+    HashSet<Vector3Int> GenerateObstacleCoordinates() {
+        Vector3Int[] coordinates = new Vector3Int[numObstacles];
+        HashSet<Vector3Int> coordinateSet = new HashSet<Vector3Int>();
+        while (coordinateSet.Count < numObstacles) {
+            int x = Random.Range(0, mapSize);
+            int y = Random.Range(0, mapSize);
+            coordinateSet.Add(new Vector3Int(x, y, 0));
+        }
+        return coordinateSet;
     }
     // Update is called once per frame
     void Update() {
